ab3769a283fd2cedaa908955a823952d925e50a9
kantaro-cloud <cqjudgchen@gmail.com>
2021-08-19 21:22:39 +0800

    update

--- system/src/main/java/cn/kantaro/SystemApplication.java
+++ system/src/main/java/cn/kantaro/SystemApplication.java
@@ -3,6 +3,7 @@ package cn.kantaro;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("cn.kantaro.mapper")
@@ -11,4 +12,5 @@ public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
     }
+
 }
